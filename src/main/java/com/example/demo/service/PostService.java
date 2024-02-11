7c96af10257ae84b2f832697c8a5ccaee6b55ca8
KarSich <hryhcev@bk.ru>
2024-02-11 15:59:48 +0300

    Add post

--- src/main/java/com/example/demo/service/PostService.java
+++ src/main/java/com/example/demo/service/PostService.java
@@ -10,11 +10,18 @@ import java.util.List;
 
 @Service
 public class PostService {
-    public List<Post> listAllPosts(){
-        Post[] posts = {new Post("Продается телевизор, купили новый. Торг уместен", new Date(2007)),
-                        new Post("Стерилизация - единственное гуманное и эффективное решение проблемы бездомных животных.", new Date(126,1,5)),
-                        new Post("Линька – естественный процесс обновления шерстного покрова, характерный для большинства животных.", new Date(116,4,5))};
+    private List<Post> posts;
+    public List<Post> listAllPosts(){return posts;}
 
-        return new ArrayList<Post>(Arrays.asList(posts));
+    public PostService(){
+        initPosts();
+    }
+    private void initPosts(){
+        posts=new ArrayList<Post>(Arrays.asList(new Post("Продается телевизор, купили новый. Торг уместен", new Date(2007)),
+                new Post("Стерилизация - единственное гуманное и эффективное решение проблемы бездомных животных.", new Date(126,1,5)),
+                new Post("Линька – естественный процесс обновления шерстного покрова, характерный для большинства животных.", new Date(116,4,5))));
+    }
+    public void create(String text) {
+        posts.add(new Post(text, new Date()));
     }
 }
